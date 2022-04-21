7baf6790d58ffcf3e9fe2140e2242e6f1b588a78
Sanjeeda Shutrishna <sanjeedatazrin@gmail.com>
2022-04-21 12:41:51 -0500

    Added a Login Page

--- app/src/main/java/com/example/wholettheclothesout/LoginActivity.kt
+++ app/src/main/java/com/example/wholettheclothesout/LoginActivity.kt
@@ -18,30 +18,49 @@ import layout.UserModal
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
+import java.net.URL
 import java.nio.charset.Charset
 
 
 private const val TAG = "LoginActivity"
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+    private lateinit var loginButton: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        private lateinit var auth: FirebaseAuth
-        // ...
-        // Initialize Firebase Auth
-        auth = Firebase.auth
         Log.d(TAG, "onCreate(Bundle?) called")
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        loginButton = findViewById(R.id.loginButton)
+
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
-    public override fun onStart() {
+
+    override fun onStart() {
         super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            reload();
-        }
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
--- app/src/main/java/com/example/wholettheclothesout/MainActivity.kt
+++ app/src/main/java/com/example/wholettheclothesout/MainActivity.kt
@@ -18,102 +18,28 @@ import layout.UserModal
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
+import java.net.URL
 import java.nio.charset.Charset
 
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var sohreButton: Button
-    private lateinit var pittmanButton: Button
-    private lateinit var noreliusButton: Button
-    private var requestQueue: RequestQueue? = null
+    private lateinit var loginButton: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
-        sohreButton = findViewById(R.id.sohreButton)
-        pittmanButton = findViewById(R.id.pittmanButton)
-        noreliusButton = findViewById(R.id.noreliusButton)
-        requestQueue = Volley.newRequestQueue(this)
+        loginButton = findViewById(R.id.loginButton)
 
 
-        sohreButton.setOnClickListener {
-            val intent = Intent(this, SohreActivity::class.java)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
 
-        pittmanButton.setOnClickListener {
-            val intent = Intent(this, PittmanActivity::class.java)
-            startActivity(intent)
-        }
-
-        noreliusButton.setOnClickListener {
-            val intent = Intent(this, NoreliusActivity::class.java)
-            startActivity(intent)
-        }
-
-        jsonParse()
-    }
-
-//    private fun getDataFromAPI() {
-//        val url = "https://opensheet.elk.sh/1o5t26He2DzTweYeleXOGiDjlU4Jkx896f95VUHVgS8U/Test+Sheet"
-//
-//        val queue = Volley.newRequestQueue(this@MainActivity)
-//        Log.d(TAG, "API called")
-//
-//        val jsonObjectRequest =
-//            JsonObjectRequest(Request.Method.GET, url, null,  Response.Listener {
-////                fun onResponse(response: JSONObject) {
-////                    loadingPB.setVisibility(View.GONE)
-//                    response ->
-//                {
-//                    try {
-//                        val feedObj = response.getJSONObject("feed")
-//                        val entryArray = feedObj.getJSONArray("entry")
-//                        for (i in 0 until entryArray.length()) {
-//                            val entryObj = entryArray.getJSONObject(i)
-//                            val machineName =
-//                                entryObj.getJSONObject("gsx\$MachineName").getString("\$t")
-//                            val availability =
-//                                entryObj.getJSONObject("gsx\$Availability").getString("\$t")
-//                            Log.d(TAG, "$i $machineName $availability")
-//                        }
-//                    } catch (e: JSONException) {
-//                        e.printStackTrace()
-//                    }
-//                }
-//            }catch(e: JSON Exception) { // handline on error listener method.
-//                Toast.makeText(this@MainActivity, "Fail to get data..", Toast.LENGTH_SHORT)
-//                    .show()
-//            }
-//        // calling a request queue method
-//        // and passing our json object
-//        // calling a request queue method
-//        // and passing our json object
-//        queue.add(jsonObjectRequest)
-//
-//    }
-    private fun jsonParse() {
-        val url = "https://opensheet.elk.sh/1o5t26He2DzTweYeleXOGiDjlU4Jkx896f95VUHVgS8U/Test+Sheet"
-        val request = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener {
-                response ->try {
-            val jsonArray = response.getJSONArray("employees")
-            for (i in 0 until jsonArray.length()) {
-                val employee = jsonArray.getJSONObject(i)
-                val machineName = employee.getString("MachineName")
-                val availability = employee.getInt("availability")
-                Log.d(TAG, "$i $machineName $availability")
-//                textView.append("$firstName, $age, $mail\n\n")
-            }
-        } catch (e: JSONException) {
-            e.printStackTrace()
-        }
-        }, Response.ErrorListener { error -> error.printStackTrace() })
-        requestQueue?.add(request)
     }
 
 
@@ -138,4 +64,3 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onDestroy() called")
     }
 }
-
