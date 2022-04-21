c77e0bbffc9c8a0890f398a6ce864ee1fdd09ea1
Beamlake Hailu <beamlake.hailu@nordstrom.com>
2022-04-21 12:52:16 -0500

    Added code to read from the data base into activity_norelius.xml in Norelius activity.

--- app/src/main/java/com/example/wholettheclothesout/NoreliusActivity.kt
+++ app/src/main/java/com/example/wholettheclothesout/NoreliusActivity.kt
@@ -4,15 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class NoreliusActivity : AppCompatActivity() {
     private lateinit var backButton: Button
     private lateinit var availabilityButton1: Button
+    private lateinit var listView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_norelius)
         backButton = findViewById(R.id.backButton)
+        listView =findViewById(R.id.listView)
         backButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
@@ -22,6 +28,11 @@ class NoreliusActivity : AppCompatActivity() {
             availabilityButton1.setEnabled(false)
             availabilityButton1.setText("IN USE")
         }
+        ArrayList<String> list = new ArrayList<>();
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item, list)
+        listView.setAdapter(adapter);
+
+        val reference : DatabaseReference = FirebaseDatabase.getInstance().getReference().child("MachineName");
 
     }
 }
